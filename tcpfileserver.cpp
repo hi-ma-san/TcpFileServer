12fdc9e61439f98daf1b9a80a95edf70de14cb1d
hi-ma-san <41143161@nfu.edu.tw>
2025-11-28 23:54:48 +0800

    1.0.0

--- tcpfileserver.cpp
+++ tcpfileserver.cpp
@@ -2,6 +2,115 @@
 
 TcpFileServer::TcpFileServer(QWidget *parent)
     : QDialog(parent)
-{}
+{
+    totalBytes = 0;
+    byteReceved = 0;
+    fileNameSize = 0;
+    serverProgressBar = new QProgressBar;
+    serverStatusLabel = new QLabel(tr("伺服器端就緒"));
+    startButton = new QPushButton(tr("接收"));
+    quitButton = new QPushButton(tr("退出"));
+    buttonBox = new QDialogButtonBox;
+    buttonBox -> addButton(startButton, QDialogButtonBox::ActionRole);
+    buttonBox -> addButton(quitButton, QDialogButtonBox::RejectRole);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    mainLayout -> addWidget(serverProgressBar);
+    mainLayout -> addWidget(serverStatusLabel);
+    mainLayout -> addStretch();
+    mainLayout -> addWidget(buttonBox);
+    setLayout(mainLayout);
+    setWindowTitle(tr("接收檔案"));
+
+    connect(startButton, SIGNAL(clicked()), this, SLOT(start()));
+    connect(quitButton, SIGNAL(clicked()), this, SLOT(close()));
+    connect(&tcpServer, SIGNAL(newConnection()), this, SLOT(acceptConnection));
+    connect(&tcpServer, SIGNAL(acceptError(QAbstractSocket::SocketError)), this, SLOT(displayError(QAbstractSocket::SocketError)));
+
+}
 
 TcpFileServer::~TcpFileServer() {}
+
+
+void TcpFileServer::start()
+{
+    byteReceved = 0;
+    fileNameSize = 0;
+    while(!tcpServer.isListening() && !tcpServer.listen(QHostAddress::AnyIPv4, 16689))
+    {
+        QMessageBox::StandardButton ret = QMessageBox::critical(this, tr("迴圈"), tr("無法啟動伺服器: %1.").arg(tcpServer.errorString()), QMessageBox::Retry | QMessageBox::Cancel);
+        if (ret == QMessageBox::Cancel) return;
+    }
+    startButton -> setEnabled(false);
+    serverStatusLabel -> setText(tr("監聽中..."));
+}
+
+void TcpFileServer::acceptConnection()
+{
+    tcpServerConnection = tcpServer.nextPendingConnection();
+    connect(tcpServerConnection, SIGNAL(readyRead()), this, SLOT(updateServerProgress()));
+    connect(tcpServerConnection, SIGNAL(error(QAbstractSocket::SocketError)), this, SLOT(displayError(QAbstractSocket::SocketError)));
+    serverStatusLabel -> setText(tr("接受連線"));
+    tcpServer.close();
+}
+
+void TcpFileServer::updateServerProgress()
+{
+    QDataStream in(tcpServerConnection);
+    in.setVersion(QDataStream::Qt_4_6);
+    if (byteReceved <= sizeof(qint64) * 2)
+    {
+        if (fileNameSize == 0 && (tcpServerConnection -> bytesAvailable() >= fileNameSize))
+        {
+            in >> fileName;
+            byteReceved += fileNameSize;
+            localFile = new QFile(fileName);
+            if (!localFile -> open(QFile::WriteOnly))
+            {
+                QMessageBox::warning(this, tr("應用程式"), tr("無法讀取檔案 %1: \n%2.").arg(fileName).arg(localFile->errorString()));
+                return;
+            }
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    if (byteReceved < totalBytes)
+    {
+        byteReceved += tcpServerConnection -> bytesAvailable();
+        inBlock = tcpServerConnection -> readAll();
+        localFile -> write(inBlock);
+        inBlock.resize(0);
+    }
+
+    serverProgressBar -> setMaximum(totalBytes);
+    serverProgressBar -> setValue(byteReceved);
+    qDebug() << byteReceved;
+    serverStatusLabel -> setText(tr("已接收 %1 Bytes").arg(byteReceved));
+
+    if (byteReceved == totalBytes)
+    {
+        tcpServerConnection -> close();
+        startButton -> setEnabled(true);
+        localFile -> fileName();
+        localFile -> close();
+        start();
+    }
+
+}
+
+
+void TcpFileServer::displayError(QAbstractSocket::SocketError socketError)
+{
+    if (socketError == QTcpSocket::RemoteHostClosedError) return;
+
+    QMessageBox::information(this, tr("網路錯誤"), tr("產生下列錯誤: %1").arg(tcpServerConnection -> errorString()));
+
+    tcpServerConnection -> close();
+    serverProgressBar -> reset();
+    serverStatusLabel -> setText(tr("伺服器就緒"));
+    startButton -> setEnabled(true);
+
+}
--- tcpfileserver.h
+++ tcpfileserver.h
@@ -2,6 +2,8 @@
 #define TCPFILESERVER_H
 
 #include <QDialog>
+#include <QtNetwork>
+#include <QtWidgets>
 
 class TcpFileServer : public QDialog
 {
@@ -10,5 +12,30 @@ class TcpFileServer : public QDialog
 public:
     TcpFileServer(QWidget *parent = nullptr);
     ~TcpFileServer();
+
+public slots:
+    void start();
+    void acceptConnection();
+    void updateServerProgress();
+    void displayError(QAbstractSocket::SocketError socketError);
+private:
+    QProgressBar        *serverProgressBar;
+    QLabel              *serverStatusLabel;
+    QPushButton         *startButton;
+    QPushButton         *quitButton;
+    QDialogButtonBox    *buttonBox;
+
+    //QtcpServer用於接收client端接收請求
+    //當QTcpServer物件接收到client請求後創建一個QTcpSocket物件與client端進行連線 (newConnection)
+    //並繼續等待其他client發起連線請求 如果有多個連線請求則會同時創建多個QTcpSocket
+    QTcpServer      tcpServer;
+    QTcpSocket      *tcpServerConnection;
+    qint64          totalBytes;
+    qint64          byteReceved;
+    qint64          fileNameSize;
+    QString         fileName;
+    QFile           *localFile;
+    QByteArray      inBlock;
+
 };
 #endif // TCPFILESERVER_H
